d3eb724623ec608729d0c4018bf941be9b831441
minggging <109min1004@naver.com>
2023-01-26 18:09:45 +0900

    Initial Commit

---
+++ TodoApp_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp_practice
+//
+//  Created by 정민경 on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
